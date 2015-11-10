893f5b517d9f17238c5906e0a5b61511230009a4
Pablo Ortega Mesa <pablo@khrno.cl>
2015-11-10 00:30:44 -0300

    Initial Commit

---
+++ Passcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Passcode
+//
+//  Created by Pablo Ortega Mesa on 10-11-15.
+//  Copyright © 2015 khrno.cl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Passcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Passcode
+//
+//  Created by Pablo Ortega Mesa on 10-11-15.
+//  Copyright © 2015 khrno.cl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
